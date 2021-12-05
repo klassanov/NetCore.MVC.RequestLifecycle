d480da5d4bd07a5371e5e794a5ea5e281bfd0074
Alexander Klassanov <klassanov@gmail.com>
2021-12-05 11:55:54 +0200

    Add project files.

---
+++ NetCore.MVC.RequestLifecycle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore.MVC.RequestLifecycle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
