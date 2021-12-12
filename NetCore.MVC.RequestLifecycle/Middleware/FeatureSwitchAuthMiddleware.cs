05aedd75561e96e3793e48fec41450f8391c82e9
Alexander Klassanov <klassanov@gmail.com>
2021-12-12 19:27:44 +0200

    Comments added

--- NetCore.MVC.RequestLifecycle/Middleware/FeatureSwitchAuthMiddleware.cs
+++ NetCore.MVC.RequestLifecycle/Middleware/FeatureSwitchAuthMiddleware.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCore.MVC.RequestLifecycle.Middleware
 {
@@ -25,17 +25,19 @@ namespace NetCore.MVC.RequestLifecycle.Middleware
             var endpoint = context.GetEndpoint();
             var endpointMetadata = endpoint?.Metadata;
             var endpontRouteAttributeMetadata = endpointMetadata?.GetMetadata<Microsoft.AspNetCore.Mvc.RouteAttribute>();
-            
 
-            if(endpontRouteAttributeMetadata != null)
+
+            if (endpontRouteAttributeMetadata != null)
             {
                 //Feature flag is disabled
                 var featureFlag = config.GetSection("FeatureFlags").GetChildren().FirstOrDefault(x => x.Key == endpontRouteAttributeMetadata.Name);
                 if (featureFlag != null && !bool.Parse(featureFlag.Value))
                 {
-                   
+
                     //ReSet a new endpoint to custom 404 middleware
                     //All other middleware is cut off as we generate a response directly
+                    //We substitute the previously selected endpoint
+                    //Actually, here, we both get and set an endpoint
                     context.SetEndpoint(new Endpoint((context) =>
                     {
                         context.Response.StatusCode = StatusCodes.Status404NotFound;
@@ -43,7 +45,7 @@ namespace NetCore.MVC.RequestLifecycle.Middleware
                     },
                     EndpointMetadataCollection.Empty,
                     "FeatureNotFound"));
-                  
+
 
                     //Alternatively, just return a response, instead of setting up an endpoint that returns a 404
                     //context.Response.StatusCode = StatusCodes.Status404NotFound;
