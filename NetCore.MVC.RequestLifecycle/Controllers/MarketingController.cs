33d9b9fdff40e69a3186d0da8abf0d0ed8908170
Alexander Klassanov <klassanov@gmail.com>
2021-12-23 10:46:03 +0200

    Mobile redirect action filter added

---
+++ NetCore.MVC.RequestLifecycle/Controllers/MarketingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.MVC.RequestLifecycle.Filters;
+
+namespace NetCore.MVC.RequestLifecycle.Controllers
+{
+    public class MarketingController : Controller
+    {
+        [MobileRedirectActionFilter(Action = "NewSplash", Controller = "Marketing")]
+        public IActionResult Splash()
+        {
+            return Content("The old splash page");
+        }
+
+        public IActionResult NewSplash()
+        {
+            return Content("The new splash page");
+        }
+
+    }
+}
---
+++ NetCore.MVC.RequestLifecycle/Filters/MobileRedirectActionFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCore.MVC.RequestLifecycle.Filters
+{
+    public class MobileRedirectActionFilter : Attribute, IActionFilter
+    {
+        //Used to pass values to the class from outside [MobileRedirectActionFilter(Action="...", Controller="...")]
+        public string Action { get; set; }
+
+        public string Controller { get; set; }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine("Action executed");
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.Headers.ContainsKey("x-mobile"))
+            {
+                //Take a look also to other filter OutageAuthorizationFilter
+                context.Result = new RedirectToActionResult(Action, Controller, null);
+            }
+        }
+    }
+}
